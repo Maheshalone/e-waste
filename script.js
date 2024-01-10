8e7cda4279addfb99f30577ffb2cbc60e05bfaf5
Maheshalone <99307716+Maheshalone@users.noreply.github.com>
2024-01-10 12:54:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ function submitImage() {
 
     if (file) {
         var fileName = file.name.split('.')[0];
-        console.log('Selected File Name:', fileName);
 
+        // Assuming jsonData is already defined and contains the JSON data
         var selectedItem = jsonData.find(item => item['item id'] === fileName);
 
         var selectedItemInfoDiv = document.getElementById('selectedItemInfo');
         if (selectedItem) {
+            // Display result
             selectedItemInfoDiv.innerHTML = `
                 <p><strong>Item ID:</strong> ${selectedItem['item id']}</p>
                 <p><strong>Item Description:</strong> ${selectedItem['item description id']}</p>
                 <p><strong>Item Image URL:</strong> ${selectedItem['item image url id']}</p>
             `;
         } else {
+            // Display message if item not found
             selectedItemInfoDiv.innerHTML = 'Item not found in the JSON data.';
         }
     } else {
+        // Alert if no image is chosen
         alert('Please choose an image.');
     }
 }
