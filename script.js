c1038245a42c0c8c1a2f051ec778972b4b3d30d7
Maheshalone <99307716+Maheshalone@users.noreply.github.com>
2024-01-10 13:04:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function submitImage() {
     var input = document.getElementById('chooseImage');
     var file = input.files[0];
-
     if (file) {
         var fileName = file.name.split('.')[0];
-
-        // Assuming jsonData is already defined and contains the JSON data
         var selectedItem = jsonData.find(item => item['item id'] === fileName);
-
         var selectedItemInfoDiv = document.getElementById('selectedItemInfo');
         if (selectedItem) {
             // Display result
